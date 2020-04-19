32f2cd3b2a9c1710037a4739faec3c4969ee0630
Krishnan Balasubramaniam <Krishnan.Balasubramaniam@dh.com>
2020-04-19 16:21:35 +0530

    organized files

--- cypress/integration/bayzatTest.spec.js
+++ cypress/integration/bayzatTest.spec.js
@@ -1,48 +1,17 @@
-const testData = require('../testData/testData.json');
+import testData from '../testData/testData.json';
+
 context('Bayzat Test', () => {
     beforeEach(() => {
-            cy.visit(testData.URL);
-            cy.contains('Login').click();
-            cy.get('[type=email]').type(testData.login_userName);
-            cy.get('[type=password]').type(testData.login_password);
-            cy.get('[type=submit]').click();
-            cy.url().should('eq', testData.dashboard_URL);
-        });
-    it('.should() - View the Team, Add and Delete Employee', () => {  
-        cy.contains('View Team').scrollIntoView().should('be.visible');
-        cy.contains('View Team').click();
-        cy.get('.js-employee-list').find('tbody tr').should('have.length.greaterThan', 0);
-        cy.contains('Add Employees').scrollIntoView().should('be.visible');
-        cy.contains('Add Employees').click();
-        cy.contains('Add Employee').click();
-        cy.get('[name=firstName]').type(testData.employee_firstName);
-        cy.get('[name=lastName]').type(testData.employee_lastName);
-        cy.get('.fa-check-square').scrollIntoView().should('be.visible');
-        cy.get('.fa-check-square').click();
-        cy.contains('Create and add another').click();
-        cy.wait(3000);
-        cy.get('[name=firstName]').type(testData.employee_firstName);
-        cy.get('[name=lastName]').type(testData.employee_lastName);
-        cy.get('.fa-check-square').scrollIntoView().should('be.visible');
-        cy.get('.fa-check-square').click();
-        cy.contains('Create').click();
-        cy.wait(3000);
-        cy.contains('View Team').scrollIntoView().should('be.visible');
-        cy.contains('View Team').click();
-        cy.get('[placeholder="Search by employee name"]').scrollIntoView().should('be.visible');
-        cy.get('[placeholder="Search by employee name"]').type(testData.employee_firstName + ' ' + testData.employee_lastName);
-        cy.wait(5000);
-        cy.get('.js-employee-list').find('tbody tr').should('have.length.greaterThan', 0);
-        cy.get('.js-employee-list').find('thead i:first').and('have.class', 'fa fa-fw fa-square-o').click({ multiple: true });
-        cy.get('.fa-trash-o').should('be.visible').click();
-        cy.contains('Confirm').should('be.visible').click();
-        cy.wait(5000);
-        cy.get('.js-employee-list').find('tbody tr td').should('have.text', 'No employees yet.');
+        cy.viewport(1366, 768);
+        cy.visit(testData.login_URL);
+        cy.bz_login(testData.login_userName, testData.login_password, testData.dashboard_URL)
+    });
+    it('.should() - View the Team, Add and Delete Employee', () => {
+        cy.bz_navigateToTeam();
+        cy.bz_addNewEmployee(testData.employeeCreationData);
+        cy.bz_searchAndDeleteEmployee(testData.employeeCreationData);
     });
     afterEach(() => {
-        cy.get('[data-external-id=logout-link]').scrollIntoView().should('be.visible');
-        cy.get('[data-external-id=logout-link]').click();
-        cy.url().should('eq', testData.login_URL);
-        cy.get('[type=submit]').should('be.visible');
-    });     
+        cy.bz_logout(testData.logout_URL);
+    });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,63 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('bz_login', (userName, password, dashURL) => {
+    cy.get('[type=email]').clear()
+        .type(userName);
+    cy.get('[type=password]').clear()
+        .type(password);
+    cy.get('[type=submit]').click();
+    cy.url().should('eq', dashURL);
+});
+Cypress.Commands.add('bz_navigateToTeam', () => {
+    cy.contains('View Team').scrollIntoView()
+        .should('be.visible');
+    cy.contains('View Team').click();
+    cy.get('.js-employee-list').find('tbody tr')
+        .should('have.length.greaterThan', 0);
+});
+Cypress.Commands.add('bz_addNewEmployee', (employeeData) => {
+    cy.contains('Add Employees').scrollIntoView()
+        .should('be.visible');
+    cy.contains('Add Employees').click();
+    cy.contains('Add Employee').click();
+    if (employeeData.length > 0) {
+        employeeData.forEach(emp => {
+            cy.get('[name=firstName]').clear()
+                .type(emp.employee_firstName);
+            cy.get('[name=lastName]').clear()
+                .type(emp.employee_lastName);
+            if (!emp.isEmailRequired) {
+                cy.get('.fa-check-square').scrollIntoView()
+                    .should('be.visible');
+                cy.get('.fa-check-square').click();
+            } else { /* TODO- Add Email Address to the respective field before saving... */ }
+            cy.contains('Create and add another').click();
+            cy.wait(3000);
+        });
+    } else { throw new Error("Test data not found!") };
+});
+Cypress.Commands.add('bz_searchAndDeleteEmployee', (employeeData) => {
+    cy.bz_navigateToTeam();
+    if (employeeData.length > 0) {
+        employeeData.forEach(emp => {
+            cy.get('[placeholder="Search by employee name"]').scrollIntoView()
+                .should('be.visible');
+            cy.get('[placeholder="Search by employee name"]').clear()
+                .type(emp.employee_firstName + ' ' + emp.employee_lastName);
+            cy.wait(5000);
+            cy.get('.js-employee-list').find('tbody tr').should('have.length.greaterThan', 0);
+            cy.get('.js-employee-list').find('tbody tr:first i:first').and('have.class', 'fa fa-fw fa-square-o').click();
+            cy.get('.fa-trash-o').should('be.visible').click();
+            cy.contains('Confirm').should('be.visible').click();
+            cy.wait(5000);
+        });
+        cy.get('.js-employee-list').find('tbody tr td').should('have.text', 'No employees yet.');
+    } else { throw new Error("Test data not found!") };
+});
+Cypress.Commands.add('bz_logout', (logoutURL) => {
+    cy.get('[data-external-id=logout-link]').scrollIntoView().should('be.visible');
+    cy.get('[data-external-id=logout-link]').click();
+    cy.url().should('eq', logoutURL);
+    cy.get('[type=submit]').should('be.visible');
+})
